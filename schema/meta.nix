2ea7f380895c0c0e1944e07c5c40c6c919424097
Nowaaru <nowaaru@proton.me>
2025-07-06 15:08:33 -0700

    fix!: schema
    i did it completely wrong. this is okay. it works now.

--- schema/meta.nix
+++ schema/meta.nix
@@ -1,89 +1,83 @@
 lib @ {
-
   mkIf,
-  mkMerge, 
+  mkMerge,
   mkEnableOption,
   mkOption,
   allSystems,
   systemImportArgs,
   ...
-}: 
-baseModule @ {config, ... }: 
-{
-  options = with lib.types; 
-  {
+}: baseModule @ {config, ...}: {
+  options = with lib.types; {
     system = mkOption {
-        type = submodule (systemModule@{ config, ...}: {
-            imports = [];
+      type = submodule (systemModule @ {config, ...}: {
+        imports = [];
 
-            options = {
-                file = mkOption {
-                    description = "The path to the system's 'default.nix' file.";
-                    type = path;
-                };
+        options = {
+          file = mkOption {
+            description = "The path to the system's 'default.nix' file.";
+            type = path;
+          };
 
-                description = mkOption {
-                  description = "The description for this System.";
-                  type = nullOr str;
-                  default = "A NixOS system.";
-                };
+          description = mkOption {
+            description = "The description for this System.";
+            type = nullOr str;
+            default = "A NixOS system.";
+          };
 
-                architecture = mkOption {
-                  description = "The system architecture that this configuration should be run on.";
-                  type = nullOr (enum allSystems);
-                  default = null;
-                };
+          architecture = mkOption {
+            description = "The system architecture that this configuration should be run on.";
+            type = nullOr (enum allSystems);
+            default = null;
+          };
 
-                specialArgs = mkOption {
-                  description = "Arguments to be layed over all modules.";
-                  type = attrs;
-                  default = {};
-                };
+          specialArgs = mkOption {
+            description = "Arguments to be layed over all modules.";
+            type = attrs;
+            default = {};
+          };
 
-                baseModules = mkOption {
-                    description = "Intrinsic modules to the system configuration.";
-                    type = listOf (oneOf [(functionTo attrs) attrs]); # TODO: fix this module type
-                };
-            };
+          baseModules = mkOption {
+            description = "Intrinsic modules to the system configuration.";
+            type = listOf (oneOf [(functionTo attrs) attrs]); # TODO: fix this module type
+          };
+        };
 
+        config.baseModules = [
+          {nixpkgs.pkgs = lib.mkForce baseModule.config.repositories.main;}
+          {nixpkgs.hostPlatform = lib.mkForce baseModule.config.system.architecture;}
+        ];
 
-            config.baseModules = [ 
-                { nixpkgs.pkgs = lib.mkForce baseModule.config.repositories.main; } 
-            ];
-
-            config.specialArgs = lib.mkMerge
-                [
-                    { 
-                        # pkgs = baseModule.config.repositories.main; 
-                        meta = baseModule;
-                    }
-                    baseModule.config.repositories.fallback
-                    systemImportArgs
-                ];
-        });
-
+        config.specialArgs =
+          lib.mkMerge
+          [
+            {
+              # pkgs = baseModule.config.repositories.main;
+              meta = baseModule.config;
+            }
+            baseModule.config.repositories.fallback
+            systemImportArgs
+          ];
+      });
     };
 
-
     packages = mkOption {
-        type = submodule (packageModule@{ config, ...}:
-        {
-            readonly = mkOption {
-                type = submodule (readonlyModule@{ config, ...}: {
-                    options.enable = mkEnableOption "readonly packages for this system.";
+      type = submodule (packageModule @ {config, ...}: {
+        readonly = mkOption {
+          type = submodule (readonlyModule @ {config, ...}: {
+            options.enable = mkEnableOption "readonly packages for this system.";
 
-                    config = mkIf readonlyModule.enable {
-                        system.baseModules = [
-                            ({}: {
-                                imports = [
-                                    inputs.gamindustri-utils.inputs.nixpkgs.nixosModules.readOnlyPkgs
-                                ];
-                            })
-                        ];         
-                    };
-                });
+            config = mkIf readonlyModule.config.enable {
+              system.baseModules = [
+                ({}: {
+                  imports = [
+                    inputs.gamindustri-utils.inputs.nixpkgs.nixosModules.readOnlyPkgs
+                  ];
+                })
+              ];
             };
-        });
+          });
+        };
+      });
     };
 
     repositories = {
