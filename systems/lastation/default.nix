2ff364133a71bd747cc64fc8b9cc5bd3800ecc08
Nowaaru <nowaaru@proton.me>
2025-07-06 15:10:27 -0700

    refactor(lastation): remove unnecessary modules
    applied automatically by meta.nix

--- systems/lastation/default.nix
+++ systems/lastation/default.nix
@@ -1,33 +1,19 @@
 toplevel @ {
-  withSystem,
   inputs,
   lib,
+  withSystem,
+  meta,
+  ...
 }: {
   flake.nixosConfigurations.lastation = withSystem "x86_64-linux" ({self', ...}: let
     pkgs = self'.legacyPackages.default;
-    specialArgs = {
-      inherit (self'.legacyPackages) nur unstable stable;
-      inherit (pkgs) lib;
-      inherit inputs;
-    };
   in
     lib.nixosSystem {
-      # inherit specialArgs;
       inherit (pkgs) lib;
 
       modules = with inputs; [
         lanzaboote.nixosModules.lanzaboote
         home-manager.nixosModules.home-manager
-        /*
-        readonly pkgs
-        */
-        # ({config, ...}: {
-        #   imports = [
-        #     inputs.gamindustri-utils.inputs.nixpkgs.nixosModules.readOnlyPkgs
-        #   ];
-        #
-        #   nixpkgs.pkgs = pkgs.lib.mkForce pkgs;
-        # })
 
         /*
         home manager
